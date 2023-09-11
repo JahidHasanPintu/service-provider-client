53a7d635cdfabf4fa7b87542ef11c0a5c2c8ece3
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-09-11 15:50:49 +0600

    login and register complete

---
+++ src/app/profile/page.js
@@ -0,0 +1,12 @@
+import Navbar from "../navbar/navbar";
+
+export default function Profile(){
+    return(
+        <div className="min-h-screen">
+            <Navbar/>
+            <h2>
+                Profile page
+            </h2>
+        </div>
+    )
+}
---
+++ src/baseURL/baseURL.js
@@ -0,0 +1,9 @@
+// const baseUrl = "https://api.dattahardware.geniushubbd.com";
+const baseUrl = "http://localhost:5000";
+
+
+const version = "api/v1";
+
+export const getApiUrl = () => {
+  return baseUrl;
+};
