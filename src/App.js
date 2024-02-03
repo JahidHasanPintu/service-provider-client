1f5af7a0ef1381c07cf67d3c0c77593e4acc9f2a
mask-shakill <mask-shakill>
2024-02-04 00:50:49 +0600

    added jobs

--- src/App.js
+++ src/App.js
@@ -1,71 +1,90 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import './i18n';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Home/Home';
-import Head from './Components/Head/Head';
-import PrivacyPolicy from './Components/Pages/PrivacyPolicy/PrivacyPolicy';
-import Contact from './Components/Pages/Contact/Contact';
-import Faq from './Components/Pages/Faq/Faq';
-import About from './Components/Pages/About/About';
-import Login from './Components/Pages/Login/Login';
-import Signup from './Components/Pages/Signup/Signup';
-import Terms from './Components/Pages/Terms/Terms';
-import Blogs from './Components/Blogs/Blogs';
-import NotFound from './Components/NotFound/NotFound';
-import BlogDetails from './Components/Blogs/BlogDetails';
-import Account from './Components/Account/Account';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import RequireAuth from './Components/RequireAuth/RequireAuth';
-import Middlebar from './Components/Head/Middlebar';
-import PostJob from './Components/PostJob/PostJob';
-import Jobs from './Components/Jobs/Jobs';
-import JobDetails from './Components/Jobs/JobDetails';
-import Products from './Components/Products/Products';
-import Checkout from './Components/Pages/Checkout/Checkout';
-import Confirmation from './Components/Pages/Checkout/Confirmation';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./i18n";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Head from "./Components/Head/Head";
+import PrivacyPolicy from "./Components/Pages/PrivacyPolicy/PrivacyPolicy";
+import Contact from "./Components/Pages/Contact/Contact";
+import Faq from "./Components/Pages/Faq/Faq";
+import About from "./Components/Pages/About/About";
+import Login from "./Components/Pages/Login/Login";
+import Signup from "./Components/Pages/Signup/Signup";
+import Terms from "./Components/Pages/Terms/Terms";
+import Blogs from "./Components/Blogs/Blogs";
+import NotFound from "./Components/NotFound/NotFound";
+import BlogDetails from "./Components/Blogs/BlogDetails";
+import Account from "./Components/Account/Account";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import RequireAuth from "./Components/RequireAuth/RequireAuth";
+import Middlebar from "./Components/Head/Middlebar";
+import PostJob from "./Components/PostJob/PostJob";
+import Jobs from "./Components/Jobs/Jobs";
+import JobDetails from "./Components/Jobs/JobDetails";
+import Products from "./Components/Products/Products";
+import Checkout from "./Components/Pages/Checkout/Checkout";
+import Confirmation from "./Components/Pages/Checkout/Confirmation";
+
 function App() {
   return (
     <div className="App bg-slate-50">
-
       <Middlebar />
       <Head />
       <Routes>
-
         <Route path="/" element={<Home />}></Route>
         <Route path="/privacy-policy" element={<PrivacyPolicy />}></Route>
         <Route path="/terms-condition" element={<Terms />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/faq" element={<Faq />}></Route>
 
-
-        <Route path="/account/*" element={<RequireAuth>
-          <Account />
-        </RequireAuth>}></Route>
+        <Route
+          path="/account/*"
+          element={
+            <RequireAuth>
+              <Account />
+            </RequireAuth>
+          }
+        ></Route>
 
         <Route path="/buy-sell" element={<Products />}></Route>
         <Route path="/checkout" element={<Checkout />}></Route>
         <Route path="/confirm" element={<Confirmation />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/jobs" element={<Jobs />}></Route>
-        <Route path="/job-details" element={<RequireAuth> <JobDetails /></RequireAuth>}></Route>
-        <Route path="/job-details/:jobID" element={<RequireAuth> <JobDetails /></RequireAuth>}></Route>
+        <Route
+          path="/job-details"
+          element={
+            <RequireAuth>
+              {" "}
+              <JobDetails />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/job-details/:jobID"
+          element={
+            <RequireAuth>
+              {" "}
+              <JobDetails />
+            </RequireAuth>
+          }
+        ></Route>
 
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/post-a-job" element={<RequireAuth><PostJob /></RequireAuth>}></Route>
-
-
-
-
-
-
+        <Route
+          path="/post-a-job"
+          element={
+            <RequireAuth>
+              <PostJob />
+            </RequireAuth>
+          }
+        ></Route>
 
         <Route path="/blogs" element={<Blogs />}></Route>
         <Route path="/blog-details" element={<BlogDetails />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
-
       </Routes>
       <Footer />
       <ToastContainer />
--- src/Components/Account/AllJobs/AllJobs.js
+++ src/Components/Account/AllJobs/AllJobs.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useOrders } from "../../../hooks/useOrders";
 import Pagination from "../../../Shared/Pagination/Pagination";
 import Loading from "../../Loading/Loading";
@@ -7,9 +7,11 @@ import { useProducts } from "../../../hooks/useProducts";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { getApiUrl } from "../../../api/apiURL";
+import EditAllJobs from "./EditAllJobs";
 
 const AllJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     fetch("http://localhost:5000/services")
       .then((res) => res.json())
@@ -57,58 +59,61 @@ const AllJobs = () => {
   };
 
   return (
-    <div>
-      <Link to={"/account/add-products"}>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 my-2 rounded">
-          Add New Job
-        </button>
-      </Link>
+    <Fragment>
+      <div>
+        <Link to={"/account/add-products"}>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 my-2 rounded">
+            Add New Job
+          </button>
+        </Link>
 
-      <div className="overflow-x-auto">
-        <table className="min-w-full border border-gray-300">
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="py-2 px-4 border-b">#</th>
-              <th className="py-2 px-4 border-b">Job Title</th>
-              <th className="py-2 px-4 border-b">Budget</th>
-              <th className="py-2 px-4 border-b">Duration</th>
-              <th className="py-2 px-4 border-b">Experience</th>
-              <th className="py-2 px-4 border-b">Type</th>
-              <th className="py-2 px-4 border-b">Action</th>
-            </tr>
-          </thead>
-          <tbody className="text-center">
-            {jobs?.map((product, index) => (
-              <tr key={product._id}>
-                <td className="py-2 px-4 border-b">{index + 1}</td>
-                <td className="py-2 px-4 border-b">{product.TITLE}</td>
-                <td className="py-2 px-4 border-b">{product.BUDGET}</td>
-                <td className="py-2 px-4 border-b">{product.DURATION}</td>
-
-                <td className="py-2 px-4 border-b">{product.EXPERIENCE}</td>
-                <td className="py-2 px-4 border-b">{product.TYPE}</td>
-                <td className="py-2 px-4 border-b">
-                  <button
-                    onClick={() => handleEdit(product)}
-                    className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 rounded mr-2"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => handleDelete(product._id)}
-                    className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded"
-                  >
-                    Delete
-                  </button>
-                </td>
+        <div className="overflow-x-auto">
+          <table className="min-w-full border border-gray-300">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="py-2 px-4 border-b">#</th>
+                <th className="py-2 px-4 border-b">Job Title</th>
+                <th className="py-2 px-4 border-b">Budget</th>
+                <th className="py-2 px-4 border-b">Duration</th>
+                <th className="py-2 px-4 border-b">Experience</th>
+                <th className="py-2 px-4 border-b">Type</th>
+                <th className="py-2 px-4 border-b">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="text-center">
+              {jobs?.map((product, index) => (
+                <tr key={product._id}>
+                  <td className="py-2 px-4 border-b">{index + 1}</td>
+                  <td className="py-2 px-4 border-b">{product.TITLE}</td>
+                  <td className="py-2 px-4 border-b">{product.BUDGET}</td>
+                  <td className="py-2 px-4 border-b">{product.DURATION}</td>
 
-        <Pagination page={page} totalPages={totalPages} setPage={setPage} />
+                  <td className="py-2 px-4 border-b">{product.EXPERIENCE}</td>
+                  <td className="py-2 px-4 border-b">{product.TYPE}</td>
+                  <td className="py-2 px-4 border-b">
+                    <button
+                      onClick={() => setShowModal(true)}
+                      className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 rounded mr-2"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(product._id)}
+                      className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <Pagination page={page} totalPages={totalPages} setPage={setPage} />
+        </div>
       </div>
-    </div>
+      <EditAllJobs isVisible={showModal} onClose={() => setShowModal(false)} />
+    </Fragment>
   );
 };
 
